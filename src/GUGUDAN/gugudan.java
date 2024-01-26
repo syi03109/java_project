404f1e73e78ae798916b69de480bbb7c8749c65d
user <syi03109@naver.com>
2024-01-26 12:43:36 +0900

    gugudan ex

--- src/GUGUDAN/gugudan.java
+++ src/GUGUDAN/gugudan.java
@@ -1,20 +1,24 @@
 package GUGUDAN;
 
+import java.util.Scanner;
+
 public class gugudan {
     public static void main(String[] args) {
-        //6단
-        int i = 1;
-        while(i < 10){
-            System.out.println(6*i);
-            i = i + 1;
+
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("사용자가 입력한 값 : " + number);
+        if (number < 2) {
+            System.out.println("값을 잘못 입력했습니다.");
+        } else if (number > 9) {
+            System.out.println("값을 잘못 입력했습니다.");
         }
 
-        //7단
-        for(int j = 1; j < 10; j++){
-            //곱셈 결과 출력
-            System.out.println(7*j);
+        for (int i = 1; i < 10; i++) {
+            System.out.println(number * i);
         }
-
 
     }
+
 }
