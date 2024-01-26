59784ca2a67382576fbbe897880be39a0a484628
user <syi03109@naver.com>
2024-01-26 12:03:07 +0900

    first commit

---
+++ src/GUGUDAN/gugudan.java
@@ -0,0 +1,20 @@
+package GUGUDAN;
+
+public class gugudan {
+    public static void main(String[] args) {
+        //6단
+        int i = 1;
+        while(i < 10){
+            System.out.println(6*i);
+            i = i + 1;
+        }
+
+        //7단
+        for(int j = 1; j < 10; j++){
+            //곱셈 결과 출력
+            System.out.println(7*j);
+        }
+
+
+    }
+}
